e9c4a992e12592e096a5b9f056aabbe6ec0db841
ErrrJohn20 <john.c@logixbpo.com>
2026-01-16 17:21:15 +0800

    navigation

--- resources/views/layouts/master-layout.blade.php
+++ resources/views/layouts/master-layout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="icon" href="{{ asset('assets/images/logix-bpo.webp') }}" type="image/webp">
     @vite([
+        'resources/css/fonticon.css',
         'resources/css/custom.css', 'resources/js/custom.js',
         'resources/css/master-layout.css', 'resources/js/master-layout.js'
         ])
@@ -15,18 +16,18 @@
   </head>
   <body>
     <nav class="main-navbar">
-      <div class="wrapper">
+      <div class="nav-container">
         <div class="nav-left">
           <ul class="menu-list">
-            <li class="menu-item">Dashboard</li>
-            <li class="menu-item">Staff Directory</li>
-            <li class="menu-item">Latest Jobs</li>
-            <li class="menu-item">Company News</li>
-            <li class="menu-item">Assigned Dashboard</li>
+            <li class="menu-item"><a class="active" href="#"><i class="ico ico-dashboard"></i>Dashboard</a></li>
+            <li class="menu-item"><a href="#"><i class="ico ico-users-add"></i>Staff Directory</a></li>
+            <li class="menu-item"><a href="#"><i class="ico ico-case"></i>Latest Jobs</a></li>
+            <li class="menu-item"><a href="#"><i class="ico ico-info"></i>Company News</a></li>
+            <li class="menu-item"><a href="#"><i class="ico ico-assign-db"></i>Assigned Dashboards</a></li>
           </ul>
         </div>
         <div class="nav-right">
-          
+          asdasdas
         </div>
       </div>
     </nav>
